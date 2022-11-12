9b5cbefa8d79d6e98da56c92d74bb73dec33ee57
Kamil Grybos <grybos.kamil@gmail.com>
2022-11-12 02:21:19 +0100

    Create initial setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# prepare raspberry environment for development or usage
+# to invoke setup.sh make sure to give file permission to execute
+# chmod +x setup.sh
+
+echo -e "\n RUNNING >>> sudo apt-get update"
+sudo apt-get update
+
+echo -e "\n RUNNING >>> sudo apt-get install unzip"
+sudo apt-get install unzip
